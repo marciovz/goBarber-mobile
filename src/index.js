706548009b84285495076b8121fc1b1bb65c0d8d
marciovz <marciovz@yahoo.com.br>
2020-01-20 08:45:20 -0300

    Primeiro commit: Configurações iniciais do react native, eslint, prettier, editorconfig

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{flx: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{color: '#000'}}>Hello world!</Text>
+    </View>
+  );
+}
